2f004c7c6a07abb7376dd5d3e61c60a6b0d34580
Cytrogen <halloweendemon3418@gmail.com>
2022-06-12 12:32:43 -0400

    Move files out of src folder

---
+++ Tester.java
@@ -0,0 +1,6 @@
+public class Tester {
+    public static void main(String[] args) {
+        int integer = Integer.parseInt("");
+        System.out.println(integer);
+    }
+}
---
+++ TextFormat.java
@@ -0,0 +1,12 @@
+public class TextFormat {
+    // public static final String variables for text formatting (color, etc.)
+    // default set of colors as tokens for the sides of the cubes
+    // can be accessed directly
+    public static final String YELLOW = "\u001b[33m";
+    public static final String RED = "\u001b[31m";
+    public static final String GREEN = "\u001b[32m";
+    public static final String BLUE = "\u001b[34m";
+    public static final String PURPLE = "\u001b[35m";
+    public static final String WHITE = "\u001b[37m";
+    public static final String RESET = "\u001b[0m";
+}
